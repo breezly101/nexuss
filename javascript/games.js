5001f69e944677616b7a8172ad7056eed3daa17b
breezly101 <16jakinyanju@mybest.org.uk>
2025-05-11 04:24:05 +0100

    Initial commit

--- javascript/games.js
+++ javascript/games.js
@@ -1,13 +1,15 @@
 const games = [
   { name: 'Bad Piggies', folder: 'games/badpiggies', icon: 'assets/images/pig.png' },
   { name: 'Eaglercraft', folder: 'games/eaglercraft', icon: 'assets/images/eaglercraft.png' },
-  { name: 'Kirka', folder: 'games/kirka', icon: 'assets/images/kirka.png' },
+  { name: 'Kirka io', folder: 'games/kirka', icon: 'assets/images/kirka.png' },
   { name: 'Slowroads', folder: 'games/slowroads', icon: 'assets/images/slowroads.png' },
   { name: 'Super Mario 64', folder: 'games/sm64', icon: 'assets/images/mario.png' },
   { name: 'Geforce NOW', folder: 'games/geforce', icon: 'assets/images/gfnow.png' },
   { name: 'Engine Sim', folder: 'games/enginesounds', icon: 'assets/images/engine.png' },
   { name: 'Basketball Bros', folder: 'games/basketball', icon: 'assets/images/basketball.png' },
   { name: 'OvO', folder: 'games/OvO', icon: 'assets/images/ovo.png' },
+  { name: 'Sonic Advance', folder: 'games/sonic advance', icon: 'assets/images/sonicadv.png' },
+  { name: 'Resent Client (eaglercraft)', folder: 'games/resent_client', icon: 'assets/images/resent.png' },
   // add more games...
 ];
 
