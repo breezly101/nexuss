a845a79c1a39f74a3748fe0e57a414ea8fa86644
breezly_ <122535095+breezly101@users.noreply.github.com>
2025-05-16 00:36:27 +0100

    Update games.js

--- javascript/games.js
+++ javascript/games.js
@@ -11,6 +11,7 @@ const games = [
   { name: 'Sonic Advance', folder: 'games/sonic advance', icon: 'assets/images/sonicadv.png' },
   { name: 'Resent Client (eaglercraft)', folder: 'games/resent_client', icon: 'assets/images/resent.png' },
   { name: 'Worlds Hardest Game', folder: 'games/hardestgame', icon: 'assets/images/hardestgame.png' },
+  { name: 'Learn To Fly', folder: 'games/learntofly', icon: 'assets/images/fly.png' },
   // add more games...
 ];
 
